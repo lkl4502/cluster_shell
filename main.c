dabf0e0e5fe77f52eab76a4815953ed71f3c81f7
오홍석 <lkl4502@kookmin.ac.kr>
2023-11-28 14:49:35 +0900

    ssh 연결 후 파이프를 통한 연결 확인

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #define R 0 // 읽기
 #define W 1 // 쓰기
 #define TOTAL_NODE 4
+#define MSGSIZE 512
 
 int main(int argc, char *argv[]) {
     int pid_arr[4];
@@ -14,6 +17,8 @@ int main(int argc, char *argv[]) {
     int fd_err[TOTAL_NODE][2];
 
     char *node_name[] = {"node1", "node2", "node3", "node4"};
+
+    char buf[MSGSIZE] = {0};
 
     for (int node = 0; node < TOTAL_NODE; node++) {
 
@@ -52,6 +57,8 @@ int main(int argc, char *argv[]) {
             // 표준 에러는 부모쪽에러 pipe에 작성한다.
             dup2(fd_err[node][W], STDERR_FILENO);
 
+            setvbuf(stdin, NULL, _IOLBF, 0);
+            setvbuf(stdout, NULL, _IOLBF, 0);
             char *exec_argv[] = {"sshpass",
                                  "-p",
                                  "ubuntu",
@@ -62,14 +69,57 @@ int main(int argc, char *argv[]) {
                                  "StrictHostKeyChecking=no",
                                  "-l",
                                  "ubuntu",
+                                 "echo Connected!",
                                  NULL};
-            execv("/bin/sshpass", exec_argv);
+            if (execv("/bin/sshpass", exec_argv) == -1) {
+                perror("execv");
+                exit(1);
+            }
             break;
 
         default: // parent process
             close(fd_to_parent[node][W]);
             close(fd_to_child[node][R]);
             close(fd_err[node][W]);
+            setvbuf(stdin, NULL, _IOLBF, 0);
+            setvbuf(stdout, NULL, _IOLBF, 0);
+        }
+    }
+
+    int n;
+    bool check_node_started[4] = {false};
+
+    while (1) {
+        for (int node = 0; node < TOTAL_NODE; node++) {
+            if (check_node_started[node])
+                continue;
+
+            switch (n = read(fd_to_parent[node][R], buf, MSGSIZE)) {
+            case -1:
+                perror("read");
+                exit(1);
+                break;
+
+            case 0:
+                printf("end");
+                exit(1);
+
+            default:
+                printf("%s : %s", node_name[node], buf);
+                memset(buf, 0, MSGSIZE);
+                check_node_started[node] = true;
+                break;
+            }
+        }
+
+        bool flag = true;
+        for (int node = 0; node < TOTAL_NODE; node++) {
+            if (!check_node_started[node])
+                flag = false;
+        }
+        if (flag) {
+            printf("연결 완료!\n");
+            break;
         }
     }
 }
